2d2425726f2823ad36b41668434160d76c4c307e
rlsouza-cyber <luisrochadesouza@gmail.com>
2021-05-13 19:09:31 -0300

    main.tf

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 3.0"
+      version = "~> 3.39.0"
     }
   }
 }
@@ -35,37 +35,24 @@ resource "aws_instance" "checkpoint2" {
     ami = "ami-0d5eff06f840b45e9"
     instance_type = "t2.micro"
     subnet_id = "subnet-0bf7eeb4e35c4959d"
-    key_name = "nightly"
-        provisioner "remote-exec" {
-        inline = [ 
-            "sudo apt-get update",
-            "sudo apt-get -y install nginx",
-            "sudo systemctl start nginx"
-        ]
-    #provisioner "file" {aws_security_group"}
-    #   source = "index.html"
-    #   destination = "/tmp/index.html"
-        connection {
-            type = "ssh"
-            user = "ubuntu"
-            private_key = file("~/.ssh/id_rsa")
-            host = self.public_ip
-        }
-      
-    }
-   provisioner "file" {
+    key_name = "nightly"   
+ provisioner "file" {
    source      = "/home/rodrigo/meuprimeirorepositorio/check_point2/index.html"
    destination = "/tmp/index.html"
    connection {
             type = "ssh"
             user = "ubuntu"
-            private_key = file("~/.ssh/id_rsa")                                                                                                                                                                                                                                                                                                                                                                                                                                                     
+            private_key = file("~/.ssh/id_rsa")
             host = self.public_ip
     }
-    }  
-    provisioner "remote-exec" {
+    }
+   provisioner "remote-exec" {
         inline = [ 
-            "sudo mv /tmp/index.html /var/www/html/index.nginx-debian.html"
+            "sudo apt-get update -y",
+            "sudo apt-get -y install nginx",
+            "sudo systemctl start nginx",
+            "sudo chmod 777 /tmp/index.html",
+            "sudo mv /tmp/index.html /var/www/html/index.html"
         ]
     connection {
             type = "ssh"
